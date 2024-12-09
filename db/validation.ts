7c240f205fef964084225a255b3280974f53a0ef
Stefan Probst <stefanprobst@posteo.de>
2024-12-09 16:41:06 +0100

    chore: wip

--- db/validation.ts
+++ db/validation.ts
@@ -1,28 +1,34 @@
-// import { createInsertSchema, createSelectSchema } from "drizzle-valibot";
+import { createInsertSchema, createSelectSchema } from "drizzle-valibot";
 
-// import {
-// 	emailVerificationRequestsTable,
-// 	passwordResetSessionsTable,
-// 	sessionsTable,
-// 	usersTable,
-// } from "@/db/schema";
+import {
+	appEventsTable,
+	emailVerificationRequestsTable,
+	passwordResetSessionsTable,
+	sessionsTable,
+	usersTable,
+	webHooksTable,
+	webHooksToAppEventsTable,
+} from "@/db/schema";
 
-/**
- * Currently not compatible with `valibot` above v0.30.
- *
- * @see https://github.com/drizzle-team/drizzle-orm/issues/2358
- */
+export const DbAppEventSchema = createSelectSchema(appEventsTable);
+export const DbAppEventInputSchema = createInsertSchema(appEventsTable);
 
-// export const DbEmailVerificationRequestSchema = createSelectSchema(emailVerificationRequestsTable);
-// export const DbEmailVerificationRequestInputSchema = createInsertSchema(
-// 	emailVerificationRequestsTable,
-// );
+export const DbEmailVerificationRequestSchema = createSelectSchema(emailVerificationRequestsTable);
+export const DbEmailVerificationRequestInputSchema = createInsertSchema(
+	emailVerificationRequestsTable,
+);
 
-// export const DbPasswordResetSessionSchema = createSelectSchema(passwordResetSessionsTable);
-// export const DbPasswordResetSessionInputSchema = createInsertSchema(passwordResetSessionsTable);
+export const DbPasswordResetSessionSchema = createSelectSchema(passwordResetSessionsTable);
+export const DbPasswordResetSessionInputSchema = createInsertSchema(passwordResetSessionsTable);
 
-// export const DbSessionSchema = createSelectSchema(sessionsTable);
-// export const DbSessionInputSchema = createInsertSchema(sessionsTable);
+export const DbSessionSchema = createSelectSchema(sessionsTable);
+export const DbSessionInputSchema = createInsertSchema(sessionsTable);
 
-// export const DbUserSchema = createSelectSchema(usersTable);
-// export const DbUserInputSchema = createInsertSchema(usersTable);
+export const DbUserSchema = createSelectSchema(usersTable);
+export const DbUserInputSchema = createInsertSchema(usersTable);
+
+export const DbWebHookSchema = createSelectSchema(webHooksTable);
+export const DbWebHookInputSchema = createInsertSchema(webHooksTable);
+
+export const DbWebHookToAppEventSchema = createSelectSchema(webHooksToAppEventsTable);
+export const DbWebHookToAppEventInputSchema = createInsertSchema(webHooksToAppEventsTable);
